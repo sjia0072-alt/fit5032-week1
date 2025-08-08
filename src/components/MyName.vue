979563953e1bcb8160ef5ef9fe80a3fc18a14fd3
Shuhong Jiang <sjia0072@student.monash.edu>
2025-08-08 16:08:47 +1000

    Initial commit

---
+++ src/components/MyName.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="name-box">
+      <h2>Hello! My name is Shuhong Jiang 👋</h2>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "MyName"
+  }
+  </script>
+  
+  <style scoped>
+  .name-box {
+    margin-top: 20px;
+    color: #42b983;
+    font-size: 1.5rem;
+  }
+  </style>
+  
