2e3b7fced4eb98577ef99c1afca1142b63b3d151
Shuhong Jiang <sjia0072@student.monash.edu>
2025-08-22 04:44:34 +1000

    vue validate

--- sjiang-library/src/components/Form.vue
+++ sjiang-library/src/components/Form.vue
@@ -7,12 +7,16 @@
                     <div class="row mb-3">
                         <div class="col-6">
                             <label for="username" class="form-label">Username</label>
-                            <input type="text" class="form-control" id="username" required v-model="formData.username">
+                            <input type="text" @blur="() => validateName(true)" @input="() => validateName(true)"
+                                class="form-control" id="username" v-model="formData.username">
+                            <div v-if="errors.username" class="text-danger">{{ errors.username }}</div>
                         </div>
                         <div class="col-6">
                             <label for="password" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="password" minlength="4" maxlength="10"
+                            <input type="password" @blur="() => validatePassword(true)"
+                                @input="() => validatePassword(true)" class="form-control" id="password"
                                 v-model="formData.password">
+                            <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
                         </div>
                     </div>
                     <div class="row mb-3">
@@ -79,14 +83,67 @@ const formData = ref({
 const submittedCards = ref([]);
 
 const submitForm = () => {
-    submittedCards.value.push({
-        ...formData.value
-    });
+    validateName(true)
+    validatePassword(true)
+    if (!errors.value.username && !errors.value.password) {
+        submittedCards.value.push({
+            ...formData.value
+        });
+        clearForm();
+    }
 };
 
 const clearForm = () => {
-    submittedCards.value = []
+    formData.value = {
+        username: '',
+        password: '',
+        isAustralian: false,
+        reason: '',
+        gender: ''
+    }
 }
+
+const errors = ref({
+    username: null,
+    password: null,
+    resident: null,
+    reason: null,
+    gender: null
+})
+
+
+const validateName = (blur) => {
+    if (formData.value.username.length < 3) {
+        if (blur) {
+            errors.value.username = "Name must be at least 3 characters";
+        }
+    } else {
+        errors.value.username = null;
+    }
+};
+
+const validatePassword = (blur) => {
+    const password = formData.value.password;
+    const minLength = 8;
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasLowercase = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+    if (password.length < minLength) {
+        if (blur) errors.value.password = `Password must be at least ${minLength} characters long.`;
+    } else if (!hasUppercase) {
+        if (blur) errors.value.password = "Password must contain at least one uppercase letter.";
+    } else if (!hasLowercase) {
+        if (blur) errors.value.password = "Password must contain at least one lowercase letter.";
+    } else if (!hasNumber) {
+        if (blur) errors.value.password = "Password must contain at least one number.";
+    } else if (!hasSpecialChar) {
+        if (blur) errors.value.password = "Password must contain at least one special character.";
+    } else {
+        errors.value.password = null;
+    }
+};
 </script>
 
 <style scoped>
